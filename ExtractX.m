73ad35c9073ace910907af719ded54e998da4735
VikWeg <vwegmayr@gmail.com>
2014-10-22 22:35:13 +0200

    Delete ExtractX.m

--- ExtractX.m
+++
@@ -1,9 +0,0 @@
-function X = ExtractX(data,selection)
-%returns Nx(# of features)
-    dim=size(data);
-    if isempty(selection)
-    X=data(1:dim(1),1:(dim(2)-1));
-    else
-    X=data(1:dim(1),selection);
-    end
-end
