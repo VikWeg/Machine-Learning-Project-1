37f55e1dded02f7dfe09dacd10af54e3a4ee9d23
VikWeg <vwegmayr@gmail.com>
2014-10-26 00:19:03 +0200

    minor changes

--- Other Scripts/LinearRegressor.m
+++ Other Scripts/LinearRegressor.m
@@ -1,13 +1,4 @@
 function [m,ev,a,b,g,UnreducedParameters]=LinearRegressor(X,T,basis,M)
-%X is Nx(# of features)
-%T is Nx1
-
-%global TrainingData ValidationData TestData
-%global unreduced
-
-%DimTraining=size(TrainingData);
-%DimValidation=size(ValidationData);
-%DimTest=size(TestData);
 
 N=length(X);
 
@@ -16,13 +7,6 @@ b=1e-6;
 g=1e-6;
 
 unreducedPhi=DesignMatrix(X,basis,M);
-Moriginal=size(unreducedPhi);
-%PhiT=Phi';%MeffxN matrix
-%PhiTPhi=(Phi')*Phi; %MeffxM matrix
-%L=eig(PhiTPhi);%Meffx1 vector
-
-%ReducePhi(DesignMatrix([X;Xvalid;Xtest],basis,M));
-%Phi=Phi(:,unreduced);
 
 [Phi,reducedV]=ReducePhi(unreducedPhi);
 
@@ -36,7 +20,7 @@ eps=1;while eps>10^-5
 
     Si=a*eye(Mreduced(2))+b*PhiTPhi;
 
-    m=Si\(b*PhiT*T);%Mx1 vector
+    m=Si\(b*PhiT*T);
 
     gold=g;
     g=sum(b*L./(b*L+a));
@@ -47,7 +31,6 @@ eps=1;while eps>10^-5
     eps=abs(g-gold)/gold;
     end
        
-
 ev=Evidence(a,b,m,Si,Phi,T);
 UnreducedParameters=Mreduced(2);
 
--- Other Scripts/PolyPredict.m
+++ Other Scripts/PolyPredict.m
@@ -3,45 +3,26 @@ function [prediction,evidence,alpha,beta,gamma,UnReducedParameters] = PolyPredic
 global TrainingData
 global ValidationData
 global TestData
-%global unreduced
 
 DimTraining=size(TrainingData);
 DimValidation=size(ValidationData);
 DimTest=size(TestData);
 
 Xtrain=TrainingData(:,1:(DimTraining(2)-1));
-%MXtrain=repmat(mean(Xtrain),DimTraining(1),1);
-%StdXtrain=repmat(std(Xtrain),DimTraining(1),1); 
-%Xtrain=exp((Xtrain-MXtrain)./StdXtrain.^1)./StdXtrain.^2;
 
 T=TrainingData(:,DimTraining(2));
 MT=repmat(mean(T),DimTraining(1),1);
 StdT=repmat(std(T),DimTraining(1),1);
 Ttrain=(T-MT)./StdT;
 
-%MXvalid=repmat(mean(ValidationData),DimValidation(1),1);
-%StdXvalid=repmat(std(ValidationData),DimValidation(1),1); 
-%Xvalid=exp((ValidationData-MXvalid)./StdXvalid.^1)./StdXvalid.^2;
-
 MX=mean([Xtrain;ValidationData;TestData]);
 StdX=std([Xtrain;ValidationData;TestData]);
+
 Xtrain=(Xtrain-repmat(MX,DimTraining(1),1))./repmat(StdX,DimTraining(1),1).^2;
 Xvalid=(ValidationData-repmat(MX,DimValidation(1),1))./repmat(StdX,DimValidation(1),1).^2;
 
 MTvalid=repmat(mean(T),DimValidation(1),1);
 StdTvalid=repmat(std(T),DimValidation(1),1);
-%{
-MXtest=repmat(mean(TestData),DimTest(1),1);
-StdXtest=repmat(std(TestData),DimTest(1),1); 
-Xtest=(TestData-MXtest)./StdXtest.^2.1;
-
-MTtest=repmat(mean(T),DimTest(1),1);
-StdTtest=repmat(std(T),DimTest(1),1);
-%}
-%[V,~]=eig(Xtrain'*Xtrain);
-
-%Vreduced=V(:,(DimValidation(2)-PCAselection+1):DimValidation(2));
-%Xreduced=Xtrain*Vreduced;
 
 [Model,evidence,alpha,beta,gamma,UnReducedParameters]=LinearRegressor(Xtrain,Ttrain,'poly',PolyOrder);
 
